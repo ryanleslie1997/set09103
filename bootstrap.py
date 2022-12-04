4767caf1e8c010611351e9942757951ed400908a
ryanleslie1997 <49491916+ryanleslie1997@users.noreply.github.com>
2022-12-04 21:25:10 +0000

    Created all pages which use template inheritence

--- bootstrap.py
+++ bootstrap.py
@@ -7,11 +7,23 @@ def root():
 
 @app.route('/home/')
 def home():
-    return render_template('inherits1.html'), 200
+    return render_template('home.html'), 200
 
 @app.route('/tourist/')
 def tourist():
-    return render_template('inherits2.html'), 200
+    return render_template('tourist.html'), 200
+
+@app.route('/retail/')
+def retail():
+    return render_template('retail.html'), 200
+
+@app.route('/hospitality/')
+def hospitality():
+    return render_template('hospitality.html'), 200
+
+@app.route('/history/')
+def history():
+    return render_template('history.html'), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
