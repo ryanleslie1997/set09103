e1c81ec1947335982572a39b628dc541e3b65e18
ryanleslie1997 <49491916+ryanleslie1997@users.noreply.github.com>
2022-12-04 23:33:40 +0000

    commit

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def root():
     return render_template('home.html'), 200
 
