4712ac491c6a4fe0218f75098f62c3bad1c70f32
ryanleslie1997 <49491916+ryanleslie1997@users.noreply.github.com>
2022-12-04 15:34:55 +0000

    Added app.route statements to Python script

--- bootstrap.py
+++ bootstrap.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def root():
     return render_template('base.html'), 200
 
+@app.route('/')
+def home():
+    return render_template('inherits1.html'), 200
+
+@app.route('/')
+def tourist():
+    return render_template('inherits2.html'), 200
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
